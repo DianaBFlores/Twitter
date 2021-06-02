afbac445b274bf49e1296363deb7385ad2798ce1
dianaBFlores <DianaBFlores@outlook.com>
2021-06-02 09:35:30 -0400

    nav bar

---
+++ src/main/java/com/tts/TechTalentTwitter/controller/FollowController.java
@@ -0,0 +1,15 @@
+package com.tts.TechTalentTwitter.controller;
+
+import com.tts.TechTalentTwitter.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class FollowController {
+    @Autowired
+    private UserService userService;
+}
